92639b15d982f29042883621c2fb874e1813a447
Akihiro.Midorikawa <gift.mido.3637@gmail.com>
2020-05-25 18:48:16 +0900

    いらないファイルの削除

--- atcorder1.py
+++
@@ -1,6 +0,0 @@
-
-
-a = int(input())
-b,c=(int(x) for x in input().split())
-d = str(input())
-print(a+b+c,d)
--- atcorder2.py
+++
@@ -1,5 +0,0 @@
-a,b = (int(x) for x in input().split())
-if a * b % 2 == 0:
-    print("Even")
-else :
-    print("odd")
